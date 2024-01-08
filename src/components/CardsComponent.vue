5415a41a514a3a5363b37bf1dfc73bc429023578
João Henrique <santos.joao@crmsolucoes.com.br>
2024-01-08 13:34:32 -0300

    Ajuste

--- src/components/CardsComponent.vue
+++ src/components/CardsComponent.vue
@@ -3,16 +3,15 @@
     <div class="row d-flex justify-content-between mT-32">
 
         <div class="col-md-6 card">
-            <div class="d-flex justify-content-center card-header">Cabeçalho</div>
-            <div class="card-body">Corpo</div>
+            <div class="d-flex justify-content-center card-header">Header</div>
+            <div class="card-body">Body</div>
         </div>
 
         <div class="col-md-6 card">
-            <div class="d-flex justify-content-center card-header">Cabeçalho</div>
-            <div class="card-body">Corpo</div>
+            <div class="d-flex justify-content-center card-header">Header</div>
+            <div class="card-body">Body</div>
         </div>
         
-
     </div>
 
 </template>
@@ -20,7 +19,20 @@
 <script>
     export default {
 
+    data() {
+        return {
+            users: [],    
+        }
+    },
+
+    mounted() {
+        fetch('http://127.0.0.1:8000/api/users')
+        .then(response => response.json())
+        .then((res) => {
+        this.users = res.data;
+        });
     }
+}
 </script>
 
 <style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -7,21 +7,58 @@
     <SearchComponent/>
 
     <CardsComponent/>
+
+  <div v-for="user in users" :key="user.id">
+    <span>{{ user.name }}</span>
+  </div>
+
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HeaderComponent from '@/components/HeaderComponent.vue'
-import SearchComponent from '@/components/SearchComponent.vue'
-import CardsComponent from '@/components/CardsComponent.vue'
+import { ref, onMounted } from 'vue';
+import HeaderComponent from '@/components/HeaderComponent.vue';
+import SearchComponent from '@/components/SearchComponent.vue';
+import CardsComponent from '@/components/CardsComponent.vue';
 
 export default {
-  name: 'HomeView',
+
   components: {
     HeaderComponent,
     SearchComponent,
     CardsComponent
+  },
+
+  setup(){
+
+  const users = ref([]);
+
+    onMounted(() => {
+      fetch('http://127.0.0.1:8000/api/users')
+        .then(response => response.json())
+        .then((res) => {
+          users.value = res.data;
+        });
+    });
+
+    return {
+      users,
+    };
+    
   }
+
+  // data() {
+  //   return {
+  //     users: [],    
+  //     }
+  // },
+
+  // mounted() {
+  //   fetch('http://127.0.0.1:8000/api/users')
+  //   .then(response => response.json())
+  //   .then((res) => {
+  //     this.users = res.data;
+  //   });
+  // }
 }
 </script>
